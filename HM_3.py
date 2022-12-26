4c6fa06f3029430dc0fffd5cb65dc20108cf98bf
Ilya <fox367213@mail.com>
2022-12-26 10:23:22 +0300

    task 2

--- HM_3.py
+++ HM_3.py
@@ -1,14 +1,32 @@
 # 1. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
-number = int(input('Введите размер списка '))
-arr = []
-sum = 0
-for i in range(number):
-     arr_number = int(input(f'Введите число {i+1} '))
-     arr.append(arr_number)
-     if i % 2 == 0:
-         sum += arr[i]
+# number = int(input('Введите размер массива '))
+# arr = []
+# sum = 0
+# for i in range(number):
+#      arr_number = int(input(f'Введите элемент {i+1} '))
+#      arr.append(arr_number)
+#      if i % 2 == 0:
+#          sum += arr[i]
+# print(arr)
+# print(f'Сумма нечетных чисел равна {sum}')
 
+# 2.Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-print(arr)
-print(f'Сумма нечетных чисел равна {sum}')
+# from random import randint
+# number = int(input('Введите размер массива '))
+# list = []
+# list2 = []
+
+# for i in range(number):
+#     list.append(randint(0, 9))
+
+# for i in range(len(list)):
+#     while i < len(list)/2 and number > len(list)/2:
+#         number = number - 1
+#         a = list[i] * list[number]
+#         list2.append(a)
+#         i += 1
+
+# print(list)
+# print(list2)
